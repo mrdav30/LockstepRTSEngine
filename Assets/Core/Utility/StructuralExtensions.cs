52dd0b78c767f5c790f0dfc0afb9b74b1116dcea
david oravsky <david.oravsky@gmail.com>
2019-10-10 20:49:17 -0400

    update: Add structural extensions to compare arrays

---
+++ Assets/Core/Utility/StructuralExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+
+namespace RTSLockstep
+{
+    public static class StructuralExtensions
+    {
+        public static bool StructuralEquals<T>(this T a, T b)
+            where T : IStructuralEquatable
+        {
+            return a.Equals(b, StructuralComparisons.StructuralEqualityComparer);
+        }
+
+        public static int StructuralCompare<T>(this T a, T b)
+            where T : IStructuralComparable
+        {
+            return a.CompareTo(b, StructuralComparisons.StructuralComparer);
+        }
+    }
+}
